3ccd4125a85088ec87fd907a6302c6fd056f1497
Mike Caron <caron.mike@outlook.com>
2020-06-19 12:17:16 -0400

    format games.js, even though it makes it a lot bigger, since it will be easier to version control

--- update.js
+++ update.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const fs = require('fs');
 const JSONL = require('json-literal');
+const jsBeautify = require('js-beautify').js;
 
 const url = 'https://itch.io/bundle/520/games.json';
 
@@ -13,9 +14,16 @@ https.get(url, res => {
 
     res.on('end', () => {
         json = json.replace(decodeURIComponent("%e2%80%a8"), ''); // the source json has a weird character that breaks things
-        json = JSON.parse(json);
-        json = JSONL.stringify(json);
+        bundle = JSON.parse(json);
+        json = JSONL.stringify(bundle, null, 2);
         let js = `// sourced from ${url}\r\n\r\nconst games = ${json};\r\n\r\n export default games;`;
+        js = jsBeautify(js);
         fs.writeFileSync('games.js', js);
+
+        /*
+        let others = bundle.games.filter(g => !g.platforms);
+        json = JSON.stringify(others, null, 2);
+        fs.writeFileSync('others.json');
+        */
     });
 })
